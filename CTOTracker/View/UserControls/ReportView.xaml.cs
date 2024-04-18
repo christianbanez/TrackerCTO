b0e64f88459333865c736d3b925092e6316cac16
dkeh <dkeh@AAD62046>
2024-04-18 14:28:04 +0800

    searchfName

--- CTOTracker/View/UserControls/ReportView.xaml.cs
+++ CTOTracker/View/UserControls/ReportView.xaml.cs
@@ -8,14 +8,19 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
+//using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation;
+//using iTextParagraph = iTextSharp.text.Paragraph;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
 
 namespace CTOTracker.View.UserControls
 {
@@ -30,90 +35,98 @@ namespace CTOTracker.View.UserControls
         {
             InitializeComponent();
             dataConnection = new DataConnection();
-            LoadScheduleData();
-            originalDtPnlHeight = dtPnl.Height;
+            LoadData();
             //PopulateComboBox();
             //cbxFilterRep.SelectionChanged += CbxFilterRep_SelectionChanged;
         }
 
-        private void LoadScheduleData()
+        private void LoadData(string fName=null)
         {
             try
             {
                 using (OleDbConnection connection = dataConnection.GetConnection())
                 {
-                    string query = "SELECT Schedule.schedID, Employee.inforID, Employee.fName, Employee.lName, Task.taskName, Role.roleName, plannedEnd, ctoEarned, dateUsed, " +
+                    string query = "SELECT Employee.inforID, Employee.fName, Employee.lName, Task.taskName, Role.roleName, plannedEnd, ctoEarned, dateUsed, " +
                                     "ctoUsed, ctoBalance FROM (((Schedule " +
                                     "LEFT JOIN Employee ON Schedule.empID = Employee.empID) " +
                                     "LEFT JOIN Role ON Employee.roleID = Role.roleID) " +
                                     "LEFT JOIN Task ON Schedule.taskID = Task.taskID);";
+                    if (!string.IsNullOrEmpty(fName))
+                    {
+                        query += " WHERE Employee.fName LIKE '%'";
+                    }
 
                     OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+                    if (!string.IsNullOrEmpty(fName))
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("?", "%" + fName + "%");
+                    }
                     DataTable dataTable = new DataTable();
                     adapter.Fill(dataTable);
-                    DataView dataView = new DataView(dataTable);
-                    scheduleDataGrid.ItemsSource = dataView;
+                    reportDataGrid.Columns.Clear();
+                    reportDataGrid.ItemsSource = dataTable.DefaultView;
+                    #region
+                    //DataView dataView = new DataView(dataTable);
+                    reportDataGrid.ItemsSource = dataTable.DefaultView;
 
                     // Create DataGrid columns
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Infor ID",
                         Binding = new Binding("inforID"),
                         Width = 75
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "First Name",
                         Binding = new Binding("fName"),
                         Width = 185
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Last Name",
                         Binding = new Binding("lName"),
                         Width = 185
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Role",
                         Binding = new Binding("roleName"),
                         Width = 125
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Task",
                         Binding = new Binding("taskName"),
                         Width = 125
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Date Earned",
                         Binding = new Binding("plannedEnd")
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "CTO Earned",
                         Binding = new Binding("ctoEarned")
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "Date Used",
                         Binding = new Binding("dateUsed")
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "CTO Used",
                         Binding = new Binding("ctoUsed")
                     });
-                    scheduleDataGrid.Columns.Add(new DataGridTextColumn
+                    reportDataGrid.Columns.Add(new DataGridTextColumn
                     {
                         Header = "CTO Balance",
                         Binding = new Binding("ctoBalance")
                     });
-
+                    #endregion
                     // Bind the DataTable to the DataGrid
-                    dataView = new DataView(dataTable);
-                    scheduleDataGrid.ItemsSource = dataView;
                 }
             }
             catch (Exception ex)
@@ -187,75 +200,106 @@ namespace CTOTracker.View.UserControls
             textBox.Text = textBox.Tag?.ToString(); // Set the placeholder text
             textBox.Foreground = Brushes.Gray;
         }
+        private void ExportToPdf(DataGrid dataGrid, string outputPath)
+        {
+            try
+            {
+                // Create a PDF document
+                Document doc = new Document();
 
-
-        /*        private void ToggleFilterPanelButton_Checked(object sender, RoutedEventArgs e)
+                // Show SaveFileDialog to get the output path
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+                saveFileDialog.FileName = "output.pdf"; // Default file name
+                if (saveFileDialog.ShowDialog() == true)
                 {
-                    filterPanel.Visibility = Visibility.Visible;
-                }
+                    outputPath = saveFileDialog.FileName;
 
-                private void ToggleFilterPanelButton_Unchecked(object sender, RoutedEventArgs e)
-                {
-                    filterPanel.Visibility = Visibility.Collapsed;
-                }*/
+                    // Proceed with PDF creation
+                    PdfWriter.GetInstance(doc, new FileStream(outputPath, FileMode.Create));
+                    doc.Open();
+                    // Add Header with Company Information
+                    PdfPTable headerTable = new PdfPTable(1);
+                    headerTable.WidthPercentage = 100;
+                    // Add current date and time
+                    DateTime currentDate = DateTime.Now;
+                    doc.Add(new Paragraph("Date generated: " + currentDate.ToString()));
+                    //doc.Add(new Paragraph.Alignment = Element.ALIGN_RIGHT);
 
-        /*        private void DataGrid_AutoGenerateColumns(object sender, EventArgs e)
-                {
-                    scheduleDataGrid.Columns[0].Visibility = Visibility.Collapsed;
-                    scheduleDataGrid.Columns[0].Header = "Schedule ID";
-                    scheduleDataGrid.Columns[1].Header = "Infor ID";
-                    scheduleDataGrid.Columns[1].Width = 75;
-                    scheduleDataGrid.Columns[2].Header = "First Name";
-                    scheduleDataGrid.Columns[2].Width = 190;
-                    scheduleDataGrid.Columns[3].Header = "Last Name";
-                    scheduleDataGrid.Columns[3].Width = 190;
-                    scheduleDataGrid.Columns[4].Header = "Task Name";
-                    scheduleDataGrid.Columns[4].Width = 125;
-                    scheduleDataGrid.Columns[5].Header = "Role";
-                    scheduleDataGrid.Columns[5].Width = 125;
-                    scheduleDataGrid.Columns[6].Header = "End Date";
-                    scheduleDataGrid.Columns[7].Header = "CTO Earned";
-                    scheduleDataGrid.Columns[8].Header = "Use Date";
-                    scheduleDataGrid.Columns[9].Header = "CTO Used";
-                    scheduleDataGrid.Columns[10].Header = "CTO Balance";
-                }
-        */
-
-        /*        private void PopulateComboBox()
-                {
-                    // Create a list of strings to populate the ComboBox
-                    List<string> filterOptions = new List<string>
+                    // Add company logo (assuming logoPath is the path to the company logo)
+                    iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(@"C:\Users\dkeh\source\repos\TrackerCTO\CTOTracker\Images\logo.png");
+                    logo.ScaleToFit(50f, 50f); // Adjust size as needed
+                    PdfPCell logoCell = new PdfPCell(logo);
+                    logoCell.HorizontalAlignment = Element.ALIGN_LEFT;
+                    logoCell.Border = PdfPCell.NO_BORDER;
+                    headerTable.AddCell(logoCell);
+                    doc.Add(new Paragraph(" "));
+                    // Add company name
+                    PdfPCell companyNameCell = new PdfPCell(new Phrase("EMPLOYEE CTO TRACKER RECORD"));
+                    companyNameCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    companyNameCell.Border = PdfPCell.NO_BORDER;
+                    headerTable.AddCell(companyNameCell);
+                    // Add empty cell to create space between header and table
+                    PdfPCell emptyCell = new PdfPCell(new Phrase(" "));
+                    emptyCell.Border = PdfPCell.NO_BORDER;
+                    headerTable.AddCell(emptyCell);
+                    doc.Add(headerTable);
+                    // Define a style for the header column
+                    Font headerFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 8, BaseColor.WHITE);
+                    Font cellFont = FontFactory.GetFont(FontFactory.HELVETICA, 7); // Adjust font size here
+                    PdfPCell headerCell = new PdfPCell();
+                    headerCell.BackgroundColor = new BaseColor(51, 122, 183); // Set background color to a shade of blue
+                    headerCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    headerCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                    headerCell.Padding = 3;
+                    // Add DataGrid content to the PDF document
+                    PdfPTable pdfTable = new PdfPTable(dataGrid.Columns.Count);
+                    pdfTable.SetWidths(new float[] { 3f, 2f, 2f, 2f, 2f, 4f, 2f, 4f, 2f, 2f }); // Adjust column widths here
+                    foreach (DataGridColumn column in dataGrid.Columns)
                     {
-                        "Option 1",
-                        "Option 2",
-                        "Option 3"
-                    };
+                        headerCell.Phrase = new Phrase(column.Header.ToString(), headerFont);
+                        pdfTable.AddCell(headerCell);
+                    }
 
-                    // Assign the list as the ItemsSource for the ComboBox
-                    cbxFilterRep.ItemsSource = filterOptions;
-                }
-
-                private void CbxFilterRep_SelectionChanged(object sender, SelectionChangedEventArgs e)
-                {
-                    // Check if a specific item is selected in the ComboBox
-                    if (cbxFilterRep.SelectedItem != null)
+                    foreach (var item in dataGrid.Items)
                     {
-                        // Get the selected item
-                        string selectedItem = cbxFilterRep.SelectedItem.ToString();
-
-                        // Check if the selected item matches the specific item
-                        if (selectedItem == "Option 2")
+                        if (item is DataRowView)
                         {
-                            // Show the Employee Filtered Panel
-                            EmpFilPnl.Visibility = System.Windows.Visibility.Visible;
-                        }
-                        else
-                        {
-                            // Hide the Employee Filtered Panel
-                            EmpFilPnl.Visibility = System.Windows.Visibility.Collapsed;
+                            DataRowView rowView = item as DataRowView;
+                            DataRow row = rowView.Row;
+                            foreach (var cell in row.ItemArray)
+                            {
+                                PdfPCell cellToAdd = new PdfPCell(new Phrase(cell.ToString(), cellFont));
+                                pdfTable.AddCell(cellToAdd);
+                            }
                         }
                     }
+                    // Set position of PDF table
+                    //pdfTable.SetTotalWidth(doc.PageSize.Width - doc.LeftMargin - doc.RightMargin);
+                    pdfTable.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                    doc.Add(pdfTable);
+                    doc.Close();
+
+                    MessageBox.Show("PDF exported successfully! Output path: " + outputPath);
                 }
-        */
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error exporting PDF: " + ex.Message);
+            }
+        }
+
+        private void btnExport_Click_1(object sender, RoutedEventArgs e)
+        {
+            ExportToPdf(reportDataGrid, null);
+        }
+
+        private void txtschFname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchFname = txtschFname.Text.Trim();
+            
+            LoadData(searchFname);
+        }
     }
 }
