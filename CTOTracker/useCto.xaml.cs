3b31801506fb5eabae15c2ff4614ae2ca0a2b56a
jsantiago3 <jsantiago3@PHHWNJSANTIAG01>
2024-04-24 11:22:32 +0800

    roletaskdone

--- CTOTracker/useCto.xaml.cs
+++ CTOTracker/useCto.xaml.cs
@@ -103,7 +103,7 @@ namespace CTOTracker
                     newRow["lName"] = row["lName"];
                     newRow["completed"] = false; // Assuming 'completed' is always false for selected rows
                     newRow["ctoEarned"] = row["ctoEarned"];
-                    newRow["ctoUsed"] = row["ctoUsed"];
+                   // newRow["ctoUsed"] = row["ctoUsed"];
                     newRow["ctoBalance"] = row["ctoBalance"];
                     selectedScheduleDataTable.Rows.Add(newRow);
                 }
